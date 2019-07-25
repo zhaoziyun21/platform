99b2df44e2e13f0834c379e8ba0fbb5e8f16be54
zhaoziyun <zhaoziyun890217@163.com>
2019-07-26 00:43:44 +0800

    细节修改

--- platform-admin/src/main/webapp/js/client/client.js
+++ platform-admin/src/main/webapp/js/client/client.js
@@ -1,11 +1,17 @@
 $(function () {
     $("#jqGrid").Grid({
         url: '../client/list',
+        postData: {
+            status: "1"
+        },
         colModel: [
             {label: '客户ID', name: 'id', index: "id", key: true, hidden: true},
+            {label: '客户姓名', name: 'clientName', index: "clientNameHide", hidden: true},
             {label: '客户姓名', name: 'clientName', width: 75, formatter: function (value, col, row) {
                          value = value == null ? '暂无' : value;
-                return '<a  onclick="vm.update('+row.id+')">'+value+'</a>' ;
+                // return "<a  onclick='vm.update(" + JSON.stringify(row) + ")'>"+value+"</a>" ;
+                // return "<a  onclick='vm.getClientInfo(" + JSON.stringify(row) + ")'>"+value+"</a>" ;
+                return value ;
             }},
             {label: '手机号', name: 'clientTel', width: 75},
             {label: '客户类型', name: 'clientType', width: 75, formatter: function (value) {
@@ -25,7 +31,18 @@ $(function () {
                 return value;
             }},
             {label: '客户经理姓名', name: 'clientManagerName', width: 75},
-            {label: '未跟单天数', name: 'noTrackOrder', width: 75},
+            {label: '未跟单天数', name: 'followTime', width: 75, formatter: function (value) {
+                value = value.replace(/-/g,'/');
+                var timestamp = new Date(value).getTime();
+                var time_diff =new Date().getTime() - timestamp ; //时间差的毫秒数
+                //计算出相差天数
+                var days = Math.floor(time_diff / (24 * 3600 * 1000));
+                if (days > 0) {
+                    return days + '天';
+                }else{
+                    return '跟进中';
+                }
+            }},
             {label: '是否上门', name: 'isVisit', width: 75, formatter: function (value) {
                 return value == 0 ? "待上门":"已上门";
             }},
@@ -40,7 +57,13 @@ $(function () {
             },
             { label: '更新时间', name: 'actionTime', index: "actionTime", width: 80, formatter: function (value) {
                 return transDate(value);
-            }}]
+            }},
+            { label: '操作',  width: 80, formatter: function (value, col, row) {
+                // return '<button class="btn btn-outline btn-info" onclick="vm.update(' + JSON.stringify(row) + ')"><i class="fa fa-info-circle"></i>&nbsp;修改</button>' ;
+                return "<a  onclick='vm.getClientInfo(" + JSON.stringify(row) + ")'>查看</a>&nbsp;&nbsp;&nbsp;&nbsp;" +
+                    "<a  onclick='vm.update(" + JSON.stringify(row) + ")'>修改</a>";
+            }}
+            ]
     });
 });
 
@@ -68,6 +91,9 @@ var vm = new Vue({
             ],
             clientType: [
                 {required: true, message: '客户类型不能为空', trigger: 'blur'}
+            ],
+            applyAmount: [
+                { pattern: /^[0-9]+(\.([0-9]{1,3}))$|^[0-9]+$/,message: '申请金额只能是整数或者小数', trigger: 'change'}
             ],
             socialSecurityPay: [
                 { pattern: /^[0-9]+(\.([0-9]{1,3}))$|^[0-9]+$/,message: '社保个缴金额只能是整数或者小数', trigger: 'change'}
@@ -103,17 +129,13 @@ var vm = new Vue({
             vm.client = {status: 1, roleIdList: [], deptId: '', deptName: ''};
 
         },
-        update: function (clientId) {
-            // var clientId = getSelectedRow("#jqGrid");
-            // if (clientId == null) {
-            //     return;
-            // }
-
+        update: function (row) {
             vm.showClientList = false;
             vm.title = "修改";
-
+            var clientId =row.id;
+            var clientName =row.realName;
             Ajax.request({
-                url: "../client/info/" + clientId,
+                url: "../client/info/" + clientId+"?clientId="+clientId+"&clientName="+clientName,
                 async: true,
                 successCallback: function (r) {
                     vm.client = r.client;
@@ -217,6 +239,14 @@ var vm = new Vue({
         },
         handleReset: function (name) {
             handleResetForm(this, name);
-        }
+        },getClientInfo: function (row) {
+            var clientId =row.id;
+            var clientName =row.clientName;
+            openWindow({
+                title: '客户详情记录',
+                type: 2,
+                content: encodeURI('../client/clientInfo.html?clientId=' + clientId + '&clientName='+clientName)
+            })
+        },
     }
 });
